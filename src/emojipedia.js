aec17f4b3d168453964a39f116189f51ebcd7bad
Aaron Park <aaronj.park0304@gmail.com>
2024-08-15 05:00:56 -0500

    Add files via upload

---
+++ src/emojipedia.js
@@ -0,0 +1,46 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g., at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Also used as a 'high five' or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of 'rofl'. Stands for 'rolling on the floor, laughing'.",
+  },
+  {
+    id: 4,
+    emoji: "😅",
+    name: "Smiling Face with Sweat",
+    meaning:
+      "Relief that something difficult is over, but also embarrassment or awkwardness. Can show happiness, relief, nervousness, or discomfort.",
+  },
+  {
+    id: 5,
+    emoji: "😍",
+    name: "Smiling Face with Heart-Eyes",
+    meaning:
+      "Shows love, adoration, or strong liking for someone or something. Often used to express excitement or admiration.",
+  },
+  {
+    id: 6,
+    emoji: "🎉",
+    name: "Party Popper",
+    meaning:
+      "Celebration or congratulations. Commonly used for birthdays, New Year's Eve, or other festive events.",
+  },
+];
+
+export default emojipedia;
